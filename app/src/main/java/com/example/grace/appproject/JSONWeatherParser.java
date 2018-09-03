f5e2d4d6b2397162c9f493b91eb7b4b290ee0756
Elvin Hwang <hosung0519@hotmail.com>
2018-09-03 16:05:13 -0700

    Changed Stuff

--- app/src/main/java/com/example/grace/appproject/JSONWeatherParser.java
+++ app/src/main/java/com/example/grace/appproject/JSONWeatherParser.java
@@ -26,7 +26,7 @@ import java.util.TimeZone;
 
 public class JSONWeatherParser {
 
-    public static Weather getWeather(String data) throws JSONException  {
+    public static Weather getWeather(String data) throws JSONException {
         Weather weather = new Weather();
         // We create out JSONObject from the data
         JSONObject jObj = new JSONObject(data);
@@ -80,113 +80,113 @@ public class JSONWeatherParser {
         Calendar now = Calendar.getInstance();
         DecimalFormat form = new DecimalFormat("00");
         int d = now.get(Calendar.DAY_OF_MONTH);
-        String day = form.format(d);
-        String exit = form.format(d+5);
+        String firstDay = form.format(d);
 
+        int index = 0;
 
-        float minTemp = 10000;
-        float maxTemp = 0;
-
-
-        DayForecast df = new DayForecast();
         WeatherForecast forecast = new WeatherForecast();
 
         // We create out JSONObject from the data
         JSONObject jObj = new JSONObject(data);
 
-        JSONArray jArr = jObj.getJSONArray("list"); // Here we have the forecast for every day
+        JSONArray jArr = jObj.getJSONArray("list");
         // We traverse all the array and parse the data
         for (int i=0; i < jArr.length(); i++) {
             JSONObject jDayForecast = jArr.getJSONObject(i);
 
             long parseDate = jDayForecast.getLong("dt")*1000;
             SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+
             //dayFormat.setTimeZone(TimeZone.getTimeZone(now.getTimeZone().getDisplayName()));
             String date = dayFormat.format(new Date(parseDate));
-            if (day.equals(date)) continue;
-            if (date.equals(exit)) break;
-
-            JSONObject jTempObj = jDayForecast.getJSONObject("main");
-            float min = (float) jTempObj.getDouble("temp_min");
-            float max = (float) jTempObj.getDouble("temp_max");
-            if (min < minTemp) minTemp = min;
-            if (max > maxTemp) maxTemp = max;
-
-            // Now we have the json object so we can extract the data
-            SimpleDateFormat hourFormat = new SimpleDateFormat("HH");
-            //hourFormat.setTimeZone(TimeZone.getTimeZone(now.getTimeZone().getDisplayName()));
-            String hour = hourFormat.format(new Date(parseDate));
-            boolean nineAM = hour.equals("08");
-            boolean threePM = hour.equals("14");
-            boolean ninePM = hour.equals("20");
-
-            if (nineAM) {
-                // Initialize DayForecast
-                df.timestamp = parseDate;
-
-                // Temp is an object
-                df.forecastTemp.minMorning = (float) jTempObj.getDouble("temp_min");
-                df.forecastTemp.maxMorning = (float) jTempObj.getDouble("temp_max");
-
-                // Humidity
-                df.weather.currentCondition.setMornHumidity((float) jTempObj.getDouble("humidity"));
-
-                // ...and now the weather
-                JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
-                JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
-                df.weather.currentCondition.setMornDescr(getString("description", jWeatherObj));
-                df.weather.currentCondition.setMornIcon(getString("icon", jWeatherObj));
-            }
-
-            if (threePM) {
-                df.forecastTemp.minAfternoon = (float) jTempObj.getDouble("temp_min");
-                df.forecastTemp.maxAfternoon = (float) jTempObj.getDouble("temp_max");
-
-                df.weather.currentCondition.setAftHumidity((float) jTempObj.getDouble("humidity"));
-
-                JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
-                JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
-                df.weather.currentCondition.setAftDescr(getString("description", jWeatherObj));
-                df.weather.currentCondition.setAftIcon(getString("icon", jWeatherObj));
-            }
-            if (ninePM) {
-                df.forecastTemp.minEvening = (float) jTempObj.getDouble("temp_min");
-                df.forecastTemp.maxEvening = (float) jTempObj.getDouble("temp_max");
-
-                df.weather.currentCondition.setEvenHumidity((float) jTempObj.getDouble("humidity"));
-
-                JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
-                JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
-                df.weather.currentCondition.setEvenDescr(getString("description", jWeatherObj));
-                df.weather.currentCondition.setEvenIcon(getString("icon", jWeatherObj));
-
-                df.forecastTemp.minTemp = minTemp;
-                df.forecastTemp.maxTemp = maxTemp;
-
-                forecast.addForecast(df);
-                df = new DayForecast();
-            }
+            if (date.equals(firstDay)) continue;
+            index = i;
+            break;
         }
+        
+        for (int i = index; i < index + 4*8; i += 8) {
+            DayForecast df = new DayForecast();
+            JSONObject jDayForecast = jArr.getJSONObject(i);
+            long parseDate = jDayForecast.getLong("dt")*1000;
+            df.timestamp = parseDate;
+            getMinMax(jArr, i, df);
 
+            jDayForecast = jArr.getJSONObject(i + 2);
+            setMorning(jDayForecast, jDayForecast.getJSONObject("main"), df);
 
+            jDayForecast = jArr.getJSONObject(i + 4);
+            setAfterNoon(jDayForecast, jDayForecast.getJSONObject("main"), df);
+
+            jDayForecast = jArr.getJSONObject(i + 6);
+            setEvening(jDayForecast, jDayForecast.getJSONObject("main"), df);
+
+            forecast.addForecast(df);
+        }
 
         return forecast;
     }
 
+    private static void setMorning(JSONObject jDayForecast, JSONObject jTempObj, DayForecast df) throws JSONException {
+        df.forecastTemp.minMorning = (float) jTempObj.getDouble("temp_min");
+        df.forecastTemp.maxMorning = (float) jTempObj.getDouble("temp_max");
+        // Humidity
+        df.weather.currentCondition.setMornHumidity((float) jTempObj.getDouble("humidity"));
+        // ...and now the weather
+        JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
+        JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
+        df.weather.currentCondition.setMornDescr(getString("description", jWeatherObj));
+        df.weather.currentCondition.setMornIcon(getString("icon", jWeatherObj));
+    }
+    private static void setAfterNoon(JSONObject jDayForecast, JSONObject jTempObj, DayForecast df) throws JSONException {
+        df.forecastTemp.minAfternoon = (float) jTempObj.getDouble("temp_min");
+        df.forecastTemp.maxAfternoon = (float) jTempObj.getDouble("temp_max");
+
+        df.weather.currentCondition.setAftHumidity((float) jTempObj.getDouble("humidity"));
+
+        JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
+        JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
+        df.weather.currentCondition.setAftDescr(getString("description", jWeatherObj));
+        df.weather.currentCondition.setAftIcon(getString("icon", jWeatherObj));
+
+    }
+    private static void setEvening(JSONObject jDayForecast, JSONObject jTempObj, DayForecast df) throws JSONException {
+        df.forecastTemp.minEvening = (float) jTempObj.getDouble("temp_min");
+        df.forecastTemp.maxEvening = (float) jTempObj.getDouble("temp_max");
+
+        df.weather.currentCondition.setEvenHumidity((float) jTempObj.getDouble("humidity"));
+
+        JSONArray jWeatherArr = jDayForecast.getJSONArray("weather");
+        JSONObject jWeatherObj = jWeatherArr.getJSONObject(0);
+        df.weather.currentCondition.setEvenDescr(getString("description", jWeatherObj));
+        df.weather.currentCondition.setEvenIcon(getString("icon", jWeatherObj));
+    }
+    private static void getMinMax(JSONArray jArr, int j, DayForecast df) throws JSONException {
+        float minTemp = 10000;
+        float maxTemp = 0;
+        for (int i=j; i < j + 8; i++) {
+            JSONObject jDayForecast = jArr.getJSONObject(i);
+            JSONObject jTempObj = jDayForecast.getJSONObject("main");
+
+            float min = (float) jTempObj.getDouble("temp_min");
+            float max = (float) jTempObj.getDouble("temp_max");
+
+            if (min < minTemp) minTemp = min;
+            if (max > maxTemp) maxTemp = max;
+        }
+        df.forecastTemp.minTemp = minTemp;
+        df.forecastTemp.maxTemp = maxTemp;
+    }
 
     private static JSONObject getObject(String tagName, JSONObject jObj)  throws JSONException {
         JSONObject subObj = jObj.getJSONObject(tagName);
         return subObj;
     }
-
     private static String getString(String tagName, JSONObject jObj) throws JSONException {
         return jObj.getString(tagName);
     }
-
     private static float  getFloat(String tagName, JSONObject jObj) throws JSONException {
         return (float) jObj.getDouble(tagName);
     }
-
     private static int  getInt(String tagName, JSONObject jObj) throws JSONException {
         return jObj.getInt(tagName);
     }
